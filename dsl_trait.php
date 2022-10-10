eb6678d150ac8e0436470993b9b7eb5cdc1efc19
bernard <bernard.ballesta@neo9.fr>
2022-10-10 20:54:44 +0200

    Saved at end of day

--- dsl_trait.php
+++ dsl_trait.php
@@ -10,7 +10,7 @@ trait dsl
     public function __call($method, $args)
         {
             $args_string = implode(", ", $args);
-            $method_name = $args[0];
+            $method_arg = $args[0];
             $class_name = get_class($this);
             echo "__call($method, [$args_string]) from $class_name Failed! \n";
             // Search failed method in current class parents
@@ -21,14 +21,15 @@ trait dsl
                     $parent_class_name = get_class($parent_class);
                     echo "Move to parent class: $parent_class_name $parent_class->name\n";
                     //die();
-                    if (method_exists($parent_class,$method_name)){
-                        echo "Found method $method_name \n";
-                        die();
+                    echo "method_exists ($parent_class_name,$method) \n";
+                    if (method_exists($parent_class,$method)){
+                        echo "Found method $method \n";
+                        //die();
                         $found_failed_method_in_parents = true;
                     }
                     else
                     {
-                        echo "Not Found method $method_name in $parent_class_name \n";
+                        echo "Not Found method $method_arg in $parent_class_name \n";
                         die();
                         // Try to see if method exists in higher level class
                         $parent_class = $parent_class->parent;
@@ -36,11 +37,12 @@ trait dsl
                 }
                 else
                 {
-                    die("*** Method  $method_name  Not found ***");
+                    // No more parent; reached tree root
+                    die("*** Method  $method_arg  Not found in parents chain ***");
                 }
             } while (!$found_failed_method_in_parents);
             echo "Now in: $parent_class->name \n";
             // Call the failed method on it's right class 
-            return $parent_class->$method_name;    
+            return $parent_class->$method($method_arg);    
         }   
 }
